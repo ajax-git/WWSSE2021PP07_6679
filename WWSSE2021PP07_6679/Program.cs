875c58731378ab833086b9164c91cd06b3e52a04
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-15 13:01:22 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP07_6679/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+   Napisz program, który dla zadanej liczby całkowitej dodatniej oblicza jej reprezentację szestnastkową.
+*/
+
+Console.WriteLine("Podaj liczbę całkowitą dodatnią: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number < 0)
+{
+    Console.WriteLine("Podaj liczbę całkowitą dodatnią: ");
+
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+string hex = number.ToString("X");
+Console.WriteLine("Reprezentacja szesnastkowa liczby {0} wynosi: {1}.", number, hex);
